b0b9b446aeff2fedb3af53fcd3dd2060092232c1
Julian Sprung <jsprung@amazon.de>
2022-09-26 15:54:41 +0200

    Simplified dependency check via utility function

--- 02_data_exploration_and_feature_eng/notebook_utilities.py
+++ 02_data_exploration_and_feature_eng/notebook_utilities.py
@@ -31,3 +31,47 @@ def cleanup_glue_resources():
          glue_client.delete_job(JobName = 'endtoendml-job')
 
     print("Cleanup completed.")
+
+def check_dependencies():
+
+    import sagemaker
+    import numpy
+    import pandas
+    
+    import sys
+    import os
+    import IPython
+
+    def versiontuple(v):
+        return tuple(map(int, (v.split("."))))
+    
+    kernel_restart_required = False
+    
+    required_version = '2.90.0'
+    if (versiontuple(sagemaker.__version__) < versiontuple(required_version)): 
+        print('This workshop was tested with sagemaker version {} but you are using {}. Installing required version...'.format(required_version, sagemaker.__version__) )
+        stream = os.popen('{} -m pip install -U sagemaker=={}'.format(sys.executable, required_version))
+        output = stream.read()
+        print(output)
+        kernel_restart_required = True
+    
+    required_version = '1.3.4'
+    if (versiontuple(pandas.__version__) < versiontuple(required_version) ):
+        print('This workshop was tested with pandas version {} but you are using {}. Installing required version...'.format(required_version, pandas.__version__))
+        stream = os.popen('{} -m pip install -U pandas=={}'.format(sys.executable, required_version))
+        output = stream.read()
+        print(output)
+        kernel_restart_required = True
+
+    required_version = '1.21.6' 
+    if (versiontuple(numpy.__version__) < versiontuple(required_version)):
+        print('This workshop was tested with numpy version {} but you are using {}. Installing required version...'.format(required_version, numpy.__version__ ) )
+        stream = os.popen('{} -m pip install -U numpy=={}'.format(sys.executable, required_version))
+        output = stream.read()
+        print(output)
+        kernel_restart_required = True
+         
+    if kernel_restart_required:
+        print("Restarting kernel after installing new dependencies...")
+        IPython.Application.instance().kernel.do_shutdown(True)     
+        print('WARNING: Kernel restarting...please wait 30 seconds before moving to the next cell!')
