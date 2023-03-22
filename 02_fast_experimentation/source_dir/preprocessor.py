1c9b83c7e30162674f308a276c02260ae6403292
Mehran Nikoo <nikoom+git@amazon.com>
2023-03-22 19:49:12 +0000

    Update experiments

--- 02_fast_experimentation/source_dir/preprocessor.py
+++ 02_fast_experimentation/source_dir/preprocessor.py
@@ -11,6 +11,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
+from sagemaker.session import Session
+from sagemaker.experiments import load_run
+
 import boto3
 
 from sklearn.exceptions import DataConversionWarning
@@ -37,17 +40,22 @@ def save_dataframe_to_file(output_path, fileName, df):
     return path
 
 
-if __name__=='__main__':
-    
-    # Read the arguments passed to the script
+def parse_args():
+
     parser = argparse.ArgumentParser()
+
     parser.add_argument('--input-data-path')
     parser.add_argument('--output-data-dir')
     parser.add_argument('--featurizer-model-dir')
     parser.add_argument('--s3-bucket-name')
     parser.add_argument('--s3-key-prefix')
+
+    return parser.parse_known_args()
+
+
+if __name__=='__main__':
     
-    args, _ = parser.parse_known_args()
+    args, _ = parse_args()
 
     input_data_path = args.input_data_path
     output_data_dir = args.output_data_dir
@@ -55,7 +63,6 @@ if __name__=='__main__':
     s3_bucket_name = args.s3_bucket_name
     s3_key_prefix = args.s3_key_prefix
     
-
     print(f'Input path is {input_data_path}')
     
     # Read input data into a Pandas dataframe
@@ -103,6 +110,24 @@ if __name__=='__main__':
     tar = tarfile.open(model_output_path, "w:gz")
     tar.add(model_joblib_path, arcname="model.joblib")
     tar.close()
+    
+    boto_session = boto3.session.Session()
+    sagemaker_session = Session(boto_session=boto_session)
+    
+    with load_run(sagemaker_session=sagemaker_session) as run:
+
+        run.log_parameters(
+        {
+          'train': training_ratio,
+          'validate': validation_ratio,
+          'test': test_ratio
+        })
+
+        run.log_artifact(name="train_data", value=train_features_output_path, media_type="text/csv", is_output=True)
+        run.log_artifact(name="val_data", value=val_features_output_path, media_type="text/csv", is_output=True)
+        run.log_artifact(name="test_data", value=test_features_output_path, media_type="text/csv", is_output=True)
+        run.log_artifact(name="featurizer_model", value=model_joblib_path, media_type="text/plain", is_output=True)
+
     
     s3 = boto3.client("s3")
     print(f'Uploading to s3 bucket "{s3_bucket_name}" prefix "{s3_key_prefix}"')
--- 02_fast_experimentation/source_dir/xgboost_training.py
+++ 02_fast_experimentation/source_dir/xgboost_training.py
@@ -10,28 +10,36 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 
-if __name__=='__main__':
+from sagemaker.session import Session
+from sagemaker.experiments import load_run
+
+def parse_args():
 
     parser = argparse.ArgumentParser()
+
     parser.add_argument('--preprocessed-data-dir')
     parser.add_argument('--model-dir')
-    parser.add_argument('--max-depth', default='3', type=int)
     parser.add_argument('--eta', default='0.1', type=float)
+    parser.add_argument('--max-depth', default=3, type=int)
     parser.add_argument('--gamma', default='0.0', type=float)
     parser.add_argument('--min-child-weight', default='1', type=int)
     parser.add_argument('--verbosity', default='0', type=int)
     parser.add_argument('--objective', default='binary:logistic')
     parser.add_argument('--eval-metric', default='auc')
     parser.add_argument('--num-boost-round', default=5, type=int)
-    
-    args, _ = parser.parse_known_args()
 
-    print('Received arguments {}'.format(args))
+    return parser.parse_known_args()
+
+if __name__=='__main__':
+
+    args, _ = parse_args()
+
+    print(f'Received arguments {args}')
     
     preprocessed_data_dir = args.preprocessed_data_dir
     model_dir = args.model_dir
-    max_depth = args.max_depth
     eta = args.eta
+    max_depth = args.max_depth
     gamma = args.gamma
     min_child_weight = args.min_child_weight
     verbosity = args.verbosity
@@ -52,66 +60,82 @@ if __name__=='__main__':
     print('Loading validation data...')
     df_val_features = pd.read_csv(val_features_path, header=None)
     df_val_labels = pd.read_csv(val_labels_path, header=None)
-    
-    X = df_train_features.values
-    y = df_train_labels.values.reshape(-1)
+
+    train_X = df_train_features.values
+    train_y = df_train_labels.values.reshape(-1)
+    print('Train features shape: {}'.format(X.shape))
+    print('Train labels shape: {}'.format(y.shape))
     
     val_X = df_val_features.values
     val_y = df_val_labels.values.reshape(-1)
-    
-    print('Train features shape: {}'.format(X.shape))
-    print('Train labels shape: {}'.format(y.shape))
     print('Validation features shape: {}'.format(val_X.shape))
     print('Validation labels shape: {}'.format(val_y.shape))
 
-    dtrain = xgboost.DMatrix(X, label=y)
+    dtrain = xgboost.DMatrix(train_X, label=train_y)
     dval = xgboost.DMatrix(val_X, label=val_y)
     watchlist = [(dtrain, "train"), (dval, "validation")]
        
-    print ('Starting training...')
+    print('')
+    print (f'===Starting training with max_depth {max_depth}===')
+
+    with load_run(sagemaker_session=sagemaker_session) as run:
+
+        param_dist = {
+            "max_depth": max_depth,
+            "eta": eta,
+            "gamma": gamma,
+            "min_child_weight": min_child_weight,
+            "verbosity": verbosity,
+            "objective": objective,
+            "eval_metric": eval_metric
+        }
+        
+        run.log_parameters(param_dist)
+            
+        xgb = xgboost.train(
+            params=param_dist,
+            dtrain=dtrain,
+            evals=watchlist,
+            num_boost_round=num_boost_round)
+
+        print('Training complete. Saving model...')
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        model_name = 'xgboost_model_{}.model'.format(str(int(time.time())))
+        model_key = 'xgboost_model{}.joblib'.format(str(int(time.time())))
+        model_path = os.path.join(model_dir, model_name)
+        model_joblib_path = os.path.join(model_dir, model_key)
+        xgb.save_model(model_path)
+        xgb.save_model(model_joblib_path)
+
+        predictions = xgb.predict(dval)
+        print ("Metrics for validation set")
+        print('')
+        print (pd.crosstab(index=val_y, columns=np.round(predictions), rownames=['Actuals'], colnames=['Predictions'], margins=True))
+        print('')
+                
+        rounded_predict = np.round(predictions)
+        
+        val_accuracy = accuracy_score(val_y, rounded_predict)
+        val_precision = precision_score(val_y, rounded_predict)
+        val_recall = recall_score(val_y, rounded_predict)
+
+        print("Accuracy Model A: %.2f%%" % (val_accuracy * 100.0))
+        print("Precision Model A: %.2f" % (val_precision))
+        print("Recall Model A: %.2f" % (1 - val_recall))
+
+        from sklearn.metrics import roc_auc_score
+
+        val_auc = roc_auc_score(val_y, predictions)
+        print("Validation AUC A: %.2f" % (val_auc))
+
+        boto_session = boto3.session.Session()
+        sagemaker_session = Session(boto_session=boto_session)    
+
+        run.log_artifact(name="model", value=model_joblib_path, media_type="text/plain", is_output=True)
+        run.log_metric(name="val_auc", value = val_auc)
+        run.log_metric(name="val_accuracy", value = val_accuracy)
+        run.log_metric(name="val_precision", value = val_precision)
+        run.log_metric(name="val_recall", value = val_recall)
+
     
-    param_dist = {
-        "max_depth": max_depth,
-        "eta": eta,
-        "gamma": gamma,
-        "min_child_weight": min_child_weight,
-        "verbosity": verbosity,
-        "objective": objective,
-        "eval_metric": eval_metric
-    }
-    xgb = xgboost.train(
-        params=param_dist,
-        dtrain=dtrain,
-        evals=watchlist,
-        num_boost_round=num_boost_round)
-
-    print('Training complete. Saving model...')
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
-    model_name = 'xgboost_model_{}.model'.format(str(int(time.time())))
-    model_key = 'xgboost_model{}.joblib'.format(str(int(time.time())))
-    model_path = os.path.join(model_dir, model_name)
-    model_joblib_path = os.path.join(model_dir, model_key)
-    xgb.save_model(model_path)
-    xgb.save_model(model_joblib_path)
-
-    predictions = xgb.predict(dval)
-    print ("===Metrics for Validation Set===")
-    print('')
-    print (pd.crosstab(index=val_y, columns=np.round(predictions), rownames=['Actuals'], colnames=['Predictions'], margins=True))
-    print('')
-    rounded_predict = np.round(predictions)
-
-    accuracy = accuracy_score(val_y, rounded_predict)
-    precision = precision_score(val_y, rounded_predict)
-    recall = recall_score(val_y, rounded_predict)
-    print('')
-
-    print("Accuracy Model A: %.2f%%" % (accuracy * 100.0))
-    print("Precision Model A: %.2f" % (precision))
-    print("Recall Model A: %.2f" % (1 - recall))
-  
-    from sklearn.metrics import roc_auc_score
-
-    auc = roc_auc_score(val_y, predictions)
-    print("AUC A: %.2f" % (auc))
